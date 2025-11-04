e4a29bb01bb18c8a454f8277b8e57b97069ecb91
Andrew Steellson <113777921+steellson@users.noreply.github.com>
2025-11-04 13:45:57 +0400

    support formats

--- Sources/Process/Converter/Converter.swift
+++ Sources/Process/Converter/Converter.swift
@@ -21,9 +21,9 @@ struct Converter {
         self.dir = dir
     }
 
-    /// Convert mp3 file to selected format using `ffmpeg`
+    /// Convert audio file to selected format using `ffmpeg`
     /// - Parameters:
-    ///   - file: Target mp3 file
+    ///   - file: Target audio file
     ///   - dir: Target directory for storage
     /// - Returns: Is convertation successed
     func convert(_ file: URL) throws {
@@ -56,17 +56,39 @@ private extension Converter {
         let outputFile = dir
             .appendingPathComponent(file.lastPathComponent)
             .deletingPathExtension()
-            .appendingPathExtension("wav")
+            .appendingPathExtension(format.rawValue)
             .path
 
-        return [
-            "-nostdin",             // Don't expect any input from stdin
-            "-y",                   // Rewrite file if exist
-            "-i", inputFile,        // Input file
-            "-ar", "44100",         // Sampling rate
-            "-ac", "2",             // 2 Channels (Stereo)
-            "-acodec", "pcm_s16le", // Wav codec
-            outputFile              // Output file
+        var args = [
+            "-nostdin",       // Don't expect any input from stdin
+            "-y",             // Rewrite file if exist
+            "-i", inputFile   // Input file
         ]
+
+        switch format {
+        case .wav:
+            args += [
+                "-ar", "44100",          // Sampling rate
+                "-ac", "2",              // 2 Channels (Stereo)
+                "-acodec", "pcm_s16le"   // WAV codec
+            ]
+        case .mp3:
+            args += [
+                "-acodec", "libmp3lame",  // MP3 codec
+                "-b:a", "320k",           // Bitrate 320 kbps (maximum quality)
+                "-q:a", "0"               // Highest quality VBR (if CBR fails)
+            ]
+        case .flac:
+            args += [
+                "-ar", "44100",            // Sampling rate (same as WAV)
+                "-ac", "2",                // 2 Channels (Stereo)
+                "-acodec", "flac",         // FLAC codec (lossless)
+                "-sample_fmt", "s16",      // 16-bit samples (same as WAV pcm_s16le)
+                "-compression_level", "8"  // Max compression (smaller size, same quality)
+            ]
+        }
+
+        args.append(outputFile)
+        return args
     }
 }
