5c64c549b5dd83ebc63c5e83089ed4e285ed5f34
Andrew Steellson <113777921+steellson@users.noreply.github.com>
2026-02-08 17:17:40 +0300

    feat: rewriting raw files disabled

--- Sources/Process/Writer/Writer.swift
+++ Sources/Process/Writer/Writer.swift
@@ -5,7 +5,7 @@ struct Writer {
 
     static func write(with name: String, file: Data) throws -> URL? {
         checkFolder()
-        let url = tempDir.appending(path: name)
+        let url = createFileURL(name)
 
         do {
             try file.write(to: url)
@@ -27,7 +27,7 @@ private extension Writer {
     static func checkFolder() {
         var isDir = ObjCBool(true)
         let isTempDirExist = FileManager.default.fileExists(
-            atPath: tempDir.path(),
+            atPath: tempDir.path(percentEncoded: false),
             isDirectory: &isDir
         )
 
@@ -44,5 +44,23 @@ private extension Writer {
         } catch {
             Console.error("Can't create temp directory: \(error)")
         }
+    }
+
+    static func createFileURL(_ name: String) -> URL {
+        let manager = FileManager.default
+        let url = tempDir.appending(path: name)
+        let path = url.path(percentEncoded: false)
+        let isDuplicate = manager.fileExists(atPath: path)
+
+        guard isDuplicate else { return url }
+        let tempPath = tempDir.path(percentEncoded: false)
+        let duplicatesCount = (try? manager
+            .contentsOfDirectory(atPath: tempPath)
+            .filter { $0.contains(name) }
+            .count) ?? 0
+
+        let numberOfDuplicate = String(duplicatesCount + 1)
+        let updatedName = name + "_" + numberOfDuplicate
+        return tempDir.appending(path: updatedName)
     }
 }
